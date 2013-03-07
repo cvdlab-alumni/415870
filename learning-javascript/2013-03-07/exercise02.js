e85b4a84dd8a9570467993b602267b0ed0cb2b3d
octawizard <roberto.manca.89@gmail.com>
2013-03-07 16:34:01 +0100

    update fibonacci

--- learning-javascript/2013-03-07/exercise02.js
+++ learning-javascript/2013-03-07/exercise02.js
@@ -1,8 +1,9 @@
 function fibonacci (n){
 	if (! (n in fibonacci)) { 	//uso la funzione come se fosse un oggetto
-		fibonacci[n] = n + fibonacci(n-1);
+		fibonacci[n] = fibonacci(n-1) + fibonacci(n-2);
 	}
 	return fibonacci[n];		//la proprietà 'n' conterrà il valore di fibonacci(n)
 }
 
 fibonacci[0] = 0; 	//condizione base della ricorsione
+fibonacci[1] = 1;
