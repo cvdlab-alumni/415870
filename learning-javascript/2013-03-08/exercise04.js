2d0ca6f62a122c6a02b95e7def5437f93b4d70f3
octawizard <roberto.manca.89@gmail.com>
2013-03-11 11:32:42 +0100

    update es4

--- learning-javascript/2013-03-08/exercise04.js
+++ learning-javascript/2013-03-08/exercise04.js
@@ -8,3 +8,14 @@ function select(data, key, values){
 	}
 	return result;
 }
+
+var data = [
+  {id:'01', name:'duffy'},
+  {id:'02', name:'michey'},
+  {id:'03', name:'donald'},
+  {id:'04', name:'goofy'},
+  {id:'05', name:'minnie'},
+  {id:'06', name:'scrooge'}
+];
+var key = 'name';
+var values = ['goofy', 'scrooge'];
